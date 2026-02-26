1582bfb9a7062bab28a48ed37b220fcb90c68303
Mewen Batty <scorpionem86@gmail.com>
2026-02-26 14:55:08 +0100

    scuffed aah base64 works for 2 chars lol

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mbatty <mbatty@student.42angouleme.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/20 12:34:33 by mbatty            #+#    #+#             */
-/*   Updated: 2026/02/25 12:30:09 by mbatty           ###   ########.fr       */
+/*   Updated: 2026/02/26 14:51:21 by mbatty           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -134,8 +134,45 @@ static int	process_stdin(t_ctx *ctx)
 	return (0);
 }
 
+#define BASE64 "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+
+void	base64(char *str)
+{
+	int	seq = 0;
+
+	uint8_t	prev = 0;
+	uint8_t	cur = *str;
+	uint8_t	b = 0;
+	size_t	i = 0;
+	(void)str;
+	while (i <= ft_strlen(str))
+	{
+		prev = cur;
+		cur = str[i];
+
+		if (seq == 0)
+			b = (cur >> 2) & 0b00111111;
+		if (seq == 1)
+			b = ((prev & 0b00000011) << 4) | ((cur & 0b11110000) >> 4);
+		if (seq == 2)
+			b = ((prev & 0b00001111) << 2) | ((cur & 0b11000000) >> 4);
+		if (seq == 3)
+			b = prev & 0b00111111;
+
+		printf("%c", BASE64[b]);
+
+		i++;
+		seq++;
+		if (seq >= 4)
+			seq = 0;
+	}
+	printf("\n");
+}
+
 int	main(int UNUSED(ac), char **av)
 {
+	base64(av[1]);
+	return (0);
 	t_ctx	ctx;
 
 	if (ctx_init(&ctx, &av) == -1)
